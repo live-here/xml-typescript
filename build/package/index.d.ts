d174f313314c6311c64395552fe12b812002c879
Heitor <hbanhete@gmail.com>
2019-04-09 21:54:13 -0300

    Exported XMLValues as default

--- build/package/index.d.ts
+++ build/package/index.d.ts
@@ -2,6 +2,7 @@ export { DEFAULT_ATTRIBUTE_PROPERTY, ns } from './lib/utils';
 export { XMLAttribute } from './lib/annotations/XMLAttribute';
 export { XMLChild } from './lib/annotations/XMLChild';
 export { XMLElement } from './lib/annotations/XMLElement';
+export { XMLValue } from './lib/annotations/XMLValue';
 export { XMLElement as xml } from './lib/models/XMLElement';
 export { XMLAttribute as xmlAttribute } from './lib/models/XMLAttribute';
 export { XMLChild as xmlChild } from './lib/models/XMLChild';
